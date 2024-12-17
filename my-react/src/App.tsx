ca58f55720d0f494ba9bc3d67408d5f8146d6af2
sergijolenuk50 <sergijolenuk50@gmail.com>
2024-12-17 23:48:49 +0200

    Try Routes

--- my-react/src/App.tsx
+++ my-react/src/App.tsx
@@ -1,21 +1,43 @@
 import { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from "./components/common/card";
+import Layout from './components/common/layout';
+import Home from './components/common/card/Home'
 
 
 function App() {
   const [count, setCount] = useState(0)
   useEffect(() => {    console.log("Use Effect!");}, []);
-console.log("Render component :)");
+// console.log("Render component :)");
 
   return (
-    <>
-    <Card/>
-      <h1 className="text-center text-3xl font-bold underline"> 
-         Hello world!
-         </h1>
+    // <>
+    // <Card/>
+    // <Layout/>
+    //   <h1 className="text-center text-3xl font-bold underline"> 
+    //      Hello world!
+    //      </h1>
+    // </>
+<>
+  {/* <Layout/> */}
+<Routes>
+<Route index element={<p>Home Page!</p>} />
+<Route  path="/" element={<Layout/>}>
+  {/* <Route index element={<p>Home Page!</p>} />
+  <Route path='/Menu' element={<MenuTable />} />
+  <Route path='/Menu/:id' element={<MenuInfo />} />
+  <Route path='/create' element={
+    <CreateMenu />
+    // <ProtectedRoute>
+    // </ProtectedRoute>
+    } />
+    <Route path='/edit/:id' element={<EditMenu />} />
+    <Route path='*' element={<p>Page Not Found!</p>} /> */}
+</Route>
+</Routes>
     </>
   )
 }
